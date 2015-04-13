5082cc1730f482ddfcac485d835bede6aff77f60
Milt0n <cedric.lood@gmail.com>
2015-04-13 21:26:40 +0200

    deployed

---
+++ form-to-email.php
@@ -0,0 +1,68 @@
+<?php
+if(!isset($_POST['submit']))
+{
+	//This page should not be accessed directly. Need to submit the form.
+	echo "error; you need to submit the form!";
+}
+if($_POST['InputReal'] != "15")
+{
+  echo "error in the verification step";
+  exit;
+}
+
+$name = $_POST['InputName'];
+$visitor_email = $_POST['InputEmail'];
+$message = $_POST['InputMessage'];
+
+//Validate first
+if(empty($name)||empty($visitor_email)) 
+{
+    echo "Name and email are mandatory!";
+    exit;
+}
+
+if(IsInjected($visitor_email))
+{
+    echo "Bad email value!";
+    exit;
+}
+
+$email_from = 'TO-SET';
+$email_subject = "New Form submission";
+$email_body = "You have received a new message from the user $name.\n".
+  "Email address of $name is $visitor_email.\n".  
+  "Here is the message:\n $message \n".
+    
+$to = "TO-SET";
+$headers = "From: $email_from \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
+//Send the email!
+mail($to,$email_subject,$email_body,$headers);
+//done. redirect to thank-you page.
+header('Location: thank-you.html');
+
+
+// Function to validate against any email injection attempts
+function IsInjected($str)
+{
+  $injections = array('(\n+)',
+              '(\r+)',
+              '(\t+)',
+              '(%0A+)',
+              '(%0D+)',
+              '(%08+)',
+              '(%09+)'
+              );
+  $inject = join('|', $injections);
+  $inject = "/$inject/i";
+  if(preg_match($inject,$str))
+    {
+    return true;
+  }
+  else
+    {
+    return false;
+  }
+}
+   
+?> 
